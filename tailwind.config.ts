12b4a0509d43d5ccf4975948ecab05953a98300f
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-03 17:21:00 +0700

    chore: setup tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,15 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        'text': 'rgb(var(--text))',
+        'background': 'rgb(var(--background))',
+        'primary': 'rgb(var(--primary))',
+        'secondary': 'rgb(var(--secondary))',
+        'accent': 'rgb(var(--accent))',
+      },
+    },
+  },
 } satisfies Config;
