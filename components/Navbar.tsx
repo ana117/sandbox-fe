0eeae7c0206a0ad6486f4191b8942f5afaad58f8
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-03 17:49:19 +0700

    refactor: formatting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,10 @@
 export default function Navbar() {
   return (
     <nav className="h-16 content-center text-center bg-background border-b-4 border-primary/50">
-      <a href="/" className="mx-auto flex items-center w-fit h-full font-bold text-text align-middle -mb-2 border-b-4 border-b-transparent hover:border-b-primary">
+      <a
+        href="/"
+        className="mx-auto flex items-center w-fit h-full font-bold text-text align-middle -mb-2 border-b-4 border-b-transparent hover:border-b-primary"
+      >
         Sandbox Frontend Edition
       </a>
     </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        'text': 'rgb(var(--text))',
-        'background': 'rgb(var(--background))',
-        'primary': 'rgb(var(--primary))',
-        'secondary': 'rgb(var(--secondary))',
-        'accent': 'rgb(var(--accent))',
+        "text": "rgb(var(--text))",
+        "background": "rgb(var(--background))",
+        "primary": "rgb(var(--primary))",
+        "secondary": "rgb(var(--secondary))",
+        "accent": "rgb(var(--accent))",
       },
     },
   },
