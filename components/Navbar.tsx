f793f2052af1592c82be737f7e10dc0f97d98994
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-03 17:21:41 +0700

    feat: add base layout and navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="h-16 content-center text-center bg-background border-b-4 border-primary/50">
+      <a href="/" className="mx-auto flex items-center w-fit h-full font-bold text-text align-middle -mb-2 border-b-4 border-b-transparent hover:border-b-primary">
+        Sandbox Frontend Edition
+      </a>
+    </nav>
+  );
+}
---
+++ routes/_layout.tsx
@@ -0,0 +1,13 @@
+import { PageProps } from "$fresh/server.ts";
+import Navbar from "../components/Navbar.tsx";
+
+export default function Layout({ Component, state }: PageProps) {
+  return (
+    <div className="min-h-screen flex flex-col text-text">
+      <Navbar />
+      <div className="flex grow bg-background p-8">
+        <Component />
+      </div>
+    </div>
+  );
+}
