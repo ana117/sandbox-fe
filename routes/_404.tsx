d1ef4c6c0378d905bf66f25744e90c008d980284
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-03 17:21:10 +0700

    feat: update 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -4,22 +4,15 @@ export default function Error404() {
   return (
     <>
       <Head>
-        <title>404 - Page not found</title>
+        <title>404 - Page Not Found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
+      <div className="m-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+          <p className="my-4">
             The page you were looking for doesn't exist.
           </p>
-          <a href="/" class="underline">Go back home</a>
+          <a href="/" className="underline">Go back home</a>
         </div>
       </div>
     </>
